e28311bd9c3747f84cefa853e1ef32ed32f1eaca
sumit <suberquercusumit@gmail.com>
2024-04-19 17:39:21 +0530

    added lecture 1 of pointers

---
+++ 06_Pointers/lecture.cpp
@@ -0,0 +1,109 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+      // int a=5;
+      // int *p = &a;
+      // cout << sizeof(p) << endl; // 🎉 output:- 8
+
+      // char ch='d';
+      // char *c = &ch;
+      // cout << sizeof(c) << endl; // 🎉 output:- 8
+
+      // double dtr=5.0881;
+      // double *d = &dtr;
+      // cout << sizeof(d) << endl; // 🎉 output:- 8
+
+
+      // 🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠
+
+      // char* ptr;
+      // cout << *ptr << endl; // 🎉output:- Segmentation fault
+
+
+      // 🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠
+        // NULL pointer
+            //       int* ptr = 0;
+            // int* ptr = nullptr;
+            //      cout << *ptr << endl; // 🎉output:- Segmentation fault
+
+      // 🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠🌠
+//   int a = 5;
+//   int b = 5;
+
+//   cout << a << endl;
+
+//   cout << &a << endl;
+//   cout << &b << endl;
+
+        // int a = 5;
+        // //pointer create
+        // int* ptr = &a;
+
+        // //access the value ptr is pointing to
+        // cout << "Address of a is: " << &a << endl;
+        // cout <<"value stored at ptr is: " << ptr << endl;
+        // cout <<"value ptr is pointing to is: " << *ptr << endl;
+        // cout << &ptr << endl;
+
+        // int a = 5;
+        // int* p = &a ;
+
+        // cout <<sizeof(p) << endl;
+
+        // char ch = 'b';
+        // char* c = &ch ;
+
+        // cout <<sizeof(c) << endl;
+
+        // double dtr = 5.03;
+        // double* d = &dtr ;
+
+        // cout <<sizeof(d) << endl;
+
+        //BAD PRACTICE
+        // int* ptr;
+
+        // cout << *ptr << endl;
+
+        //NULL POINTER
+        // int* ptr = 0;
+
+        // cout << *ptr << endl;
+        
+        // int a = 5;
+
+        // int* ptr = &a;
+
+        // //copy pointer
+        // int* dusraPtr = ptr;
+
+        // cout << *ptr << endl;
+        // cout << *dusraPtr << endl;
+ 
+
+        int a = 10;
+        int* p = &a;
+        int* q = p;
+        int* r = q;
+
+        cout << a << endl; //10
+        cout << &a << endl; //address of a 
+        cout << p << endl; //address of a 
+        cout << &p << endl; //address of p
+        cout << *p << endl; //10
+        cout << q << endl; //address of a
+        cout << &q << endl; //address of q
+        cout << *q << endl; //10
+        cout << r << endl; //address of a
+        cout << &r << endl; //address of r
+        cout << *r << endl; //10
+        cout << (*p + *q + *r) << endl; //30
+        cout << (*p)*2 + (*r)*3 << endl; //50
+        cout << (*p/2) - (*q)/2 << endl; //0
+
+
+      return 0;
+}
